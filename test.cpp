52c568405762f59010240727db0b4229bbf237bd
jahwah1 <148506495+jahwah1@users.noreply.github.com>
2023-11-16 21:55:25 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+ #include<iostream>
+#include<string>
+using namespace std;
+
+int main() 
+{      
+	cout<<"              A____A   "<<endl;
+	cout<<"             /*    *\   "<<endl;
+	cout<<"            {  >   < }"<<endl;
+	cout<<"           / !!!!!!!\ "<<endl;
+	cout<<"           / ! \!!!!!|                                        / \\ _ "<<endl;
+	cout<<"      ____{    ) | |  |                                     (o、 o 7"<<endl;
+	cout<<"     / ___{   /  | |  |                                      |  ~\  )  ~"<<endl;
+	cout<<"    { (___ \__\__@@_)@_)                                       (_.(_,_)//"<<endl;
+	cout<<"     \____)"<<endl;
+return 0;
+}
